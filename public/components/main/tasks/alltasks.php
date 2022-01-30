3c2db19ab2e3cda0a9fc21cd2a128d91c9e3b480
0xyzone <sumnsth@gmail.com>
2022-01-30 10:07:54 +0545

    added alltasks view

--- public/components/main/tasks/alltasks.php
+++ public/components/main/tasks/alltasks.php
@@ -1,12 +1,62 @@
+<!-- Queries -->
+
+<?php
+
+?>
+
+<!-- end Queries -->
+
+<!-- Check to see if alltask is called -->
 <?php if (!isset($_GET['alltask'])) : ?>
     <?php echo "You are not allowed to view this."; ?>
 <?php else : ?>
-<!-- title -->
-<script>
-    var title = 'All tasks';
-</script>
-<div class="w-full container mx-auto text-center bg-slate-200">
-    All task view coming soon! Please stay tuned.
-</div>
-<!-- end title -->
+
+
+
+    <!-- title -->
+    <script>
+        var title = 'All tasks';
+    </script>
+    <!-- end title -->
+
+
+    <!-- <div class="w-full container mx-auto text-center bg-slate-200">
+            All task view coming soon! Please stay tuned.
+        </div> -->
+    <div class="maincontent">
+        <div class="header z-[50] fadeInTop">
+            <button onclick="history.go(-1)"><i class="fad fa-arrow-left"></i></button>
+            <h1>All tasks list</h1>
+        </div>
+        <div class="flex gap-4 fadeInTop">
+            <button class="<?php if(isset($_GET['list'])){ echo 'btn-secondary'; } else { echo 'btn-primary hover:shadow-none hover:scale-100'; } ?>" id="all" <?php if(isset($_GET['list'])){ echo ""; } else { echo "disabled"; } ?> >All</button>
+            <button class="<?php if((isset($_GET['list']) && ($_GET['list'] == "byyou") )){ echo "btn-primary hover:shadow-none hover:scale-100"; } else { echo 'btn-secondary'; } ?>" id="byyou" <?php if((isset($_GET['list'])) && ($_GET['list'] == "byyou")){ echo "disabled"; } else { echo ""; } ?>>Tasks you assigned</button>
+            <button class="<?php if((isset($_GET['list']) && ($_GET['list'] == "toyou") )){ echo "btn-primary hover:shadow-none hover:scale-100"; } else { echo 'btn-secondary'; } ?>" id="toyou" <?php if((isset($_GET['list'])) && ($_GET['list'] == "toyou")){ echo "disabled"; } else { echo ""; } ?>>Assigned to you</button>
+        </div>
+        <div class="flex flex-col gap-4">
+            <?php
+            if (!isset($_GET['list'])){
+                include 'all.php';
+            } else {
+                if ($_GET['list'] == "toyou"){
+                    include 'toyou.php';
+                } else if($_GET['list'] == "byyou"){
+                    include 'byyou.php';
+                }
+            }
+            ?>
+        </div>
+    </div>
+    <script>
+        $('#toyou').click(function(){
+            location.href = mainsite+'?alltask='+user+'&list=toyou';
+        })
+        $('#byyou').click(function(){
+            location.href = mainsite+'?alltask='+user+'&list=byyou';
+        })
+        $('#all').click(function(){
+            location.href = mainsite+'?alltask='+user;
+        })
+    </script>
+
 <?php endif; ?>
---
+++ public/components/main/tasks/toyou.php
@@ -0,0 +1,31 @@
+<?php
+$alltaskquery = mysqli_query($db,"SELECT * FROM tasks WHERE assigned_to='$user'");
+// $alltaskres = mysqli_fetch_assoc($alltaskquery);
+?>
+<div class="flex flex-wrap gap-4 w-full mt-2">
+    <?php if(mysqli_num_rows($alltaskquery) < 1 ): ?>
+        <div class="flex flex-col w-full border border-stone-300 dark:border-stone-400 p-4 rounded-lg shadow-main relative fadeInBottom">
+            <h2 class="text-lg font-bold dark:text-lime-400 truncate">No tasks found!</h2>
+            <p>You haven't been assigned to any tasks yet! <span class="font-bold text-lime-700 dark:text-lime-500"></span></p>
+        </div>
+    <?php else : ?>
+    <?php foreach ($alltaskquery as $row) : ?>
+        <div class="flex flex-col w-full border border-stone-300 dark:border-stone-400 p-4 rounded-lg shadow-main relative fadeInBottom">
+            <h2 class="text-lg font-bold dark:text-lime-400 truncate"><?php echo $row['task_subject']; ?></h2>
+            <p>Assigned to: <span class="font-bold text-lime-700 dark:text-lime-500"><?php echo $row['assigned_to']; ?></span></p>
+            <div class="flex gap-2 justify-between w-full items-center">
+                <span class="text-xs dark:text-stone-100/30 text-stone-500"><?php echo $row['created_on']; ?></span>
+                <div class="flex gap-3">
+                    <a href="<?php echo '?tasks=' . $user . '&viewtask=' . $row['ID']; ?>" class="">
+                        <i class="fas fa-eye text-stone-500 hover:text-lime-700 hover:dark:text-lime-600" title="View note"></i>
+                    </a>
+                    <a href="<?php echo '?tasks=' . $user . '&delete=' . $row['ID']; ?>" class="" onclick="return confirm('Are you sure you want to delete?')">
+                        <i class="fas fa-trash text-red-500/50 hover:text-red-500 hover:dark:text-red-500" title="View note"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    <?php endforeach; ?>
+    <?php endif; ?>
+</div>
