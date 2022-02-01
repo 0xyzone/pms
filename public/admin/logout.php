e5d0bbc9ca5f07e6130a2406f5b78d9852036e46
0xyzone <sumnsth@gmail.com>
2022-02-01 09:50:06 +0545

    Update logout.php

--- public/admin/logout.php
+++ public/admin/logout.php
@@ -2,6 +2,9 @@
 include "../includes/globalvar.php";
 include "../includes/header.php";
 include "../includes/dbconnection.php";
+if ((!isset($_SESSION['dh_user']) && (isset($_COOKIE['dh_user'])))){
+    $_SESSION['dh_user'] = $_COOKIE['dh_user'];
+}
 $uname= $_SESSION['dh_user'];
 $db->query("UPDATE userbase SET status='Offline' WHERE username='$uname'");
 setcookie("dh_user",$_SESSION['dh_user'],time(), "/");
